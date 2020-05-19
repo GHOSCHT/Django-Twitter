a4fb6f9927c4718f60ae1f3fb4c91e10e9f9603d
Ghost <31184695+GHOSCHT@users.noreply.github.com>
2020-05-19 16:02:08 +0200

    Add rough views + username input -> cookies

--- twitterAPI/urls.py
+++ twitterAPI/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("set_user", views.set_user, name="set_user"),
+    path("profile", views.profile, name="profile"),
+    path("followers", views.followers, name="followers"),
+    path("follows", views.follows, name="follows")
 ]
--- twitterAPI/views.py
+++ twitterAPI/views.py
@@ -1,7 +1,53 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+from . import models
 
-# Create your views here.
+from . import twitterLogin as loginData
+import tweepy
+import datetime
+
+# Custom function
+
+
+def apiLogin():
+    auth = tweepy.OAuthHandler(
+        loginData.consumer_key, loginData.consumer_secret)
+    auth.set_access_token(loginData.access_token,
+                          loginData.access_token_secret)
+    api = tweepy.API(auth)
+    return api
+
+# Views
 
 
 def home(request):
     return render(request, "twitterAPI/home.html")
+
+
+def set_user(request):
+    try:
+        username = request.POST.get("username")
+
+        response = HttpResponseRedirect("profile")
+        response.set_cookie("twitter_username", username)
+
+    except:
+        response = render(request, "error.html")
+
+    return response
+
+
+def profile(request):
+    try:
+        username = request.COOKIES.get('twitter_username')
+    except:
+        username = "N/A"
+    return render(request, "twitterAPI/profile.html", {"username": username})
+
+
+def followers(request):
+    return render(request, "twitterAPI/followers.html")
+
+
+def follows(request):
+    return render(request, "twitterAPI/follows.html")
