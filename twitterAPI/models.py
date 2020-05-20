dfa8efa648f27c952802bef8b8e3af561827d0e3
Ghost <31184695+GHOSCHT@users.noreply.github.com>
2020-05-20 15:02:23 +0200

    Add last searched users

--- twitterAPI/models.py
+++ twitterAPI/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class TwitterUser(models.Model):
+    username = models.CharField(max_length=15)
+    search_date = models.DateTimeField(auto_now=True)
--- twitterAPI/views.py
+++ twitterAPI/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
-from . import models
+from twitterAPI.models import TwitterUser
 
 from . import twitterLogin as loginData
 import tweepy
@@ -17,11 +17,39 @@ def apiLogin():
     api = tweepy.API(auth)
     return api
 
+
+def getUser(username):
+    api = apiLogin()
+    user = api.get_user(username)
+
+    if username[0] == "@":
+        username = username.replace("@", "")
+
+    try:
+        profile_banner_url = user.profile_banner_url
+        profile_avatar_url = user.profile_image_url.replace("_normal", "")
+    except:
+        profile_banner_url = "https://images.hdqwalls.com/download/abstract-minimal-blur-5k-jj-2560x1440.jpg"
+        profile_avatar_url = "https://abs.twimg.com/sticky/default_profile_images/default_profile_400x400.png"
+
+    user_data = {"username": username,
+                 "display_name": user.name,
+                 "description": user.description,
+                 "profile_banner_url": profile_banner_url,
+                 "profile_avatar_url": profile_avatar_url,
+                 "follower_count": user.followers_count,
+                 "follow_count": user.friends_count,
+                 "user_timeline": api.user_timeline(username, count=100),
+                 }
+    return user_data
+
 # Views
 
 
 def home(request):
-    return render(request, "twitterAPI/home.html")
+    searched_users = TwitterUser.objects.all().order_by("-search_date")[:9]
+
+    return render(request, "twitterAPI/home.html", {"searched_users": searched_users})
 
 
 def set_user(request):
@@ -30,6 +58,12 @@ def set_user(request):
         response = HttpResponseRedirect("profile")
         response.set_cookie("twitter_username", username)
 
+        try:
+            apiLogin().get_user(username)
+            TwitterUser.objects.create(username=username)
+        except:
+            response = HttpResponseRedirect("/")
+
     except:
         response = render(request, "error.html")
 
@@ -37,28 +71,8 @@ def set_user(request):
 
 
 def profile(request):
-    try:
-        username = request.COOKIES.get('twitter_username').lower()
-
-    except:
-        return HttpResponseRedirect("/")
-
-    if username[0] == "@":
-        username = username.replace("@", "")
-
-    api = apiLogin()
-    user = api.get_user(username)
-
-    user_data = {"username": username,
-                 "display_name": user.name,
-                 "description": user.description,
-                 "profile_banner_url": user.profile_banner_url,
-                 "profile_avatar_url": user.profile_image_url.replace("_normal", ""),
-                 "follower_count": user.followers_count,
-                 "follow_count": user.friends_count,
-                 "user_timeline": api.user_timeline(username, count=100),
-                 }
-
+    username = request.COOKIES.get('twitter_username').lower()
+    user_data = getUser(username)
     return render(request, "twitterAPI/profile.html", user_data)
 
 
