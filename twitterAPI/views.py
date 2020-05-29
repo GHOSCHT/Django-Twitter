893b903fc330ad44cf03172dde1ad8338525dbdc
Ghost <31184695+GHOSCHT@users.noreply.github.com>
2020-05-29 18:49:27 +0200

    change username storage

--- twitterAPI/views.py
+++ twitterAPI/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, HttpResponse
-from twitterAPI.models import TwitterUser
 
 from . import twitterLogin as loginData
 import tweepy
@@ -18,12 +17,35 @@ def apiLogin():
     return api
 
 
-def getUser(username):
+def validateUser(username):
+    try:
+        api = apiLogin()
+        user = api.get_user(username)
+        return True
+    except:
+        return False
+
+
+def getUsername(request):
+    if "username" in request.session:
+        return request.session["username"]
+    else:
+        return None
+
+
+def setUser(request, username):
+    request.session["username"] = username.lower()
+
+
+def replaceInvalidCharacters(username):
+    return username.replace("@", "")
+
+
+def getUserData(username):
     api = apiLogin()
     user = api.get_user(username)
 
-    if username[0] == "@":
-        username = username.replace("@", "")
+    username = replaceInvalidCharacters(username)
 
     try:
         profile_banner_url = user.profile_banner_url
@@ -39,52 +61,34 @@ def getUser(username):
                  "profile_avatar_url": profile_avatar_url,
                  "follower_count": user.followers_count,
                  "follow_count": user.friends_count,
-                 "user_timeline": api.user_timeline(username, count=100),
+                 "user_timeline": api.user_timeline(username, count=200),
                  }
     return user_data
 
 
-def checkCookies(request):
-    try:
-        username = request.COOKIES.get('twitter_username').lower()
-        return username
-    except:
-        return None
-
 # Views
 
-
 def home(request):
-    searched_users = TwitterUser.objects.all().order_by("-search_date")[:9]
-
-    return render(request, "twitterAPI/home.html", {"searched_users": searched_users})
+    return render(request, "twitterAPI/home.html")
 
 
-def set_user(request, urlUsername):
+def set_user(request):
+    username = request.POST.get("twitterHandle")
 
-    if urlUsername == "form":
-        username = request.POST.get("twitterHandle")
+    if validateUser(username):
+        setUser(request, username)
+        response = HttpResponseRedirect("profile")
     else:
-        username = urlUsername
-
-    if username[0] == "@":
-        username = username.replace("@", "")
-
-    try:
-        apiLogin().get_user(username)
-        TwitterUser.objects.create(username=username)
-        response = HttpResponseRedirect("/profile")
-        response.set_cookie("twitter_username", username)
-
-    except:
         response = HttpResponseRedirect("/")
 
     return response
 
 
 def profile(request):
-    username = checkCookies(request)
+    username = getUsername(request)
+
     if(username == None):
         return HttpResponseRedirect("/")
-    user_data = getUser(username)
+
+    user_data = getUserData(username)
     return render(request, "twitterAPI/profile.html", user_data)
