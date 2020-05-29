d75acd5687408c830b2219fcccaf8123f4b79e13
Ghost <31184695+GHOSCHT@users.noreply.github.com>
2020-05-29 18:49:14 +0200

    remove search history

--- twitterAPI/urls.py
+++ twitterAPI/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("set_user/<str:urlUsername>", views.set_user, name="set_user"),
+    path("set_user", views.set_user, name="set_user"),
     path("profile", views.profile, name="profile"),
 ]
